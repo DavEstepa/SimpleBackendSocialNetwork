8fa27a1f6a0ca25d37fd76bf23b5dbb3602dce47
David Estepa <david.estepa98@gmail.com>
2023-08-08 11:20:31 -0500

    Added general skeleton's App

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placeRoutes = require('./routes/place-routes');
+const userRoutes = require('./routes/user-routes');
+
+const app = express()
+
+app.use('/api/places', placeRoutes); //Middleware for handle '/api/places/..' starting routes.
+app.use('/api/users', userRoutes);
+
+app.listen(5000);
