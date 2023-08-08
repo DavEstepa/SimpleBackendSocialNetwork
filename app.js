f0053302e5b84399b794ac7ec924ca86d4ddd862
David Estepa <david.estepa98@gmail.com>
2023-08-08 11:54:49 -0500

    Added general place controller logic

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/user-routes');
 
 const app = express()
 
+app.use(bodyParser.json());
 app.use('/api/places', placeRoutes); //Middleware for handle '/api/places/..' starting routes.
 app.use('/api/users', userRoutes);
 
--- routes/place-routes.js
+++ routes/place-routes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
+const placeController = require('../controllers/place-controller');
+
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.json('<api/places/> Works!!')
-});
+router.get('/:pid', placeController.getPlaceById);
+
+router.get('/user/:uid', placeController.getPlacesByUserId);
+
+router.post(
+  '/',
+  placeController.createPlace
+);
+
+router.patch(
+  '/:pid',
+  placeController.updatePlace
+);
+
+router.delete('/:pid', placeController.deletePlace);
 
 module.exports = router;
